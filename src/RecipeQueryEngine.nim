1cf84cc8e2d5e1c6aa6a6081e8b0e3f4f6f2f6a7
JohanMillberg <johanmillberg@me.com>
2025-01-12 22:22:41 +0100

    added some typees

--- src/RecipeQueryEngine.nim
+++ src/RecipeQueryEngine.nim
@@ -1,4 +1,14 @@
 import db_connector/db_sqlite
 
+# Add recipe
+
+# Update recipe
+
+# Delete recipe
+
+# Search recipes
+
+# List recipes
+
 when isMainModule:
-  echo("Hello, World!")
+  echo "Hello world!"
---
+++ src/types.nim
@@ -0,0 +1,19 @@
+type
+  Ingredient* = object
+    id*: int
+    recipeId*: int
+    name*: string
+    amount*: int
+    unit*: string
+
+  Tag* = object
+    id*: int
+    name*: string
+
+  Recipe* = object
+    id*: int
+    title*: string
+    ingredients*: seq[Ingredient]
+    instructions*: seq[string]
+    preparationTime*: int
+    tags*: seq[Tag]
