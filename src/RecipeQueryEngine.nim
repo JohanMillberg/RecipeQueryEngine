deb16644af5299128335a8bac317b3de2348eac9
Hugo Granström <5092565+HugoGranstrom@users.noreply.github.com>
2025-01-13 21:29:47 +0100

    Implement db sugar and addRecipe method (#1)
    * implement sugar templates for db
    
    * use sugar and implement addRecipe method
    
    * add example recipe to db

--- src/RecipeQueryEngine.nim
+++ src/RecipeQueryEngine.nim
@@ -11,8 +11,24 @@ import cligen
 
 # List recipes
 
+proc addExampleRecipe() =
+  let exampleRecipe = Recipe(
+    title: "Test recipe",
+    instructions: @["Instruction1", "Instruction2"],
+    preparationTime: 20,
+    servings: 4,
+    tags: @[Tag(name: "Tag1"), Tag(name: "Tag2")],
+    ingredients: @[
+      Ingredient(name: "Sugar", amount: 2, unit: "dl"),
+      Ingredient(name: "Eggs", amount: 1000)
+    ]
+  )
+  exampleRecipe.addRecipe
+
 when isMainModule:
   initializeDatabase()
+  clearDatabase()
+  addExampleRecipe()
   let recipes = getRecipeList()
   for recipe in recipes:
     echo "Recipe: ", recipe.title
--- src/db.nim
+++ src/db.nim
@@ -7,106 +7,197 @@ import ./types
 
 const databasePath = "data/recipes.db"
 
+template withTransaction*(db: DbConn, body: untyped) =
+  db.exec(sql"BEGIN")
+  try:
+    body
+  except Exception as e:
+    db.exec(sql"ROLLBACK")
+    raise e
+  db.exec(sql"COMMIT")
+
+template withConnection*(variableName: untyped, body: untyped) =
+  let `variableName` {.inject.} = open(databasePath, "", "", "")
+  try:
+    body
+  finally:
+    variableName.close()
+
+template withDb*(variableName: untyped, body: untyped) =
+  ## Combines `withConnection` and `withTransaction` for convinience
+  withConnection variableName:
+    withTransaction variableName:
+      body
+
+proc clearDatabase*() =
+  withDb db_conn:
+    for table in ["Ingredients", "Tags", "RecipeHasTag", "Recipes"]:
+      db_conn.exec(sql"""DELETE FROM ?""", table)
+
 
 proc initializeDatabase*() =
-  let db_conn = open(databasePath, "", "", "")
-  let ingredientsInitQuery = sql"""
-    CREATE TABLE IF NOT EXISTS Ingredients (
-      id       INTEGER PRIMARY KEY,
-      name     TEXT NOT NULL,
-      recipeId INTEGER NOT NULL,
-      amount   INTEGER NOT NULL,
-      unit     TEXT
-    )
-  """
-  db_conn.exec(ingredientsInitQuery)
+  withDb db_conn:
+    let ingredientsInitQuery = sql"""
+      CREATE TABLE IF NOT EXISTS Ingredients (
+        id       INTEGER PRIMARY KEY,
+        name     TEXT NOT NULL,
+        recipeId INTEGER NOT NULL,
+        amount   INTEGER NOT NULL,
+        unit     TEXT
+      )
+    """
+    db_conn.exec(ingredientsInitQuery)
 
-  let tagInitQuery = sql"""
-    CREATE TABLE IF NOT EXISTS Tags (
-      id   INTEGER PRIMARY KEY,
-      name TEXT NOT NULL
-    )
-  """
-  db_conn.exec(tagInitQuery)
+    let tagInitQuery = sql"""
+      CREATE TABLE IF NOT EXISTS Tags (
+        id   INTEGER PRIMARY KEY,
+        name TEXT NOT NULL UNIQUE
+      )
+    """
+    db_conn.exec(tagInitQuery)
 
-  let recipeTagInitQuery = sql"""
-    CREATE TABLE IF NOT EXISTS RecipeHasTag (
-      id       INTEGER PRIMARY KEY,
-      recipeId INTEGER NOT NULL,
-      tagId    INTEGER NOT NULL
-    )
-  """
-  db_conn.exec(recipeTagInitQuery)
+    let recipeTagInitQuery = sql"""
+      CREATE TABLE IF NOT EXISTS RecipeHasTag (
+        id       INTEGER PRIMARY KEY,
+        recipeId INTEGER NOT NULL,
+        tagId    INTEGER NOT NULL
+      )
+    """
+    db_conn.exec(recipeTagInitQuery)
 
-  let recipeInitQuery = sql"""
-    CREATE TABLE IF NOT EXISTS Recipes (
-      id            INTEGER PRIMARY KEY,
-      title         TEXT NOT NULL,
-      instructions  TEXT NOT NULL,
-      timeInMinutes INTEGER,
-      servings      INTEGER
-    )
-  """
-  db_conn.exec(recipeInitQuery)
-
-  db_conn.close()
+    let recipeInitQuery = sql"""
+      CREATE TABLE IF NOT EXISTS Recipes (
+        id            INTEGER PRIMARY KEY,
+        title         TEXT NOT NULL,
+        instructions  TEXT NOT NULL,
+        timeInMinutes INTEGER,
+        servings      INTEGER
+      )
+    """
+    db_conn.exec(recipeInitQuery)
 
 proc getRecipeList*(): seq[Recipe] =
-  let db_conn = open(databasePath, "", "", "")
-  let getRecipesQuery = sql"""
-  WITH IngredientLists AS (
+  withDb db_conn:
+    let getRecipesQuery = sql"""
+    WITH IngredientLists AS (
+      SELECT
+          recipeId
+        , json_group_array(
+            json_object(
+            'id', id,
+            'recipeId', recipeId,
+            'name', name,
+            'amount', amount,
+            'unit', unit
+            )
+          ) as ingredients
+      FROM Ingredients
+      GROUP BY recipeId
+    ),
+    TagList AS (
+      SELECT
+          rht.recipeId
+        , json_group_array(
+            json_object(
+              'id', t.id,
+              'name', t.name
+            )
+          ) as tags
+      FROM RecipeHasTag rht
+      INNER JOIN Tags t on t.id = rht.tagId
+      GROUP BY rht.recipeId
+    )
     SELECT
-        recipeId
-      , json_group_array(
-          json_object(
-           'name', name,
-           'amount', amount,
-           'unit', unit
-          )
-        ) as ingredients
-    FROM Ingredients
-    GROUP BY recipeId
-  ),
-  TagList AS (
-    SELECT
-        rht.recipeId
-      , json_group_array(
-          json_object(
-            'id', t.id,
-            'name', t.name
-          )
-        ) as tags
-    FROM RecipeHasTag rht
-    INNER JOIN Tags t on t.id = rht.tagId
-    GROUP BY rht.recipeId
-  )
-  SELECT
-     r.id
-   , r.title
-   , r.timeInMinutes
-   , r.instructions
-   , r.servings
-   , il.ingredients
-   , COALESCE(tl.tags, '[]') as tags
-  FROM Recipes r
-  INNER JOIN IngredientLists il ON il.recipeId = r.id
-  INNER JOIN TagList tl ON tl.recipeId = r.id
-  """
+      r.id
+    , r.title
+    , r.timeInMinutes
+    , r.instructions
+    , r.servings
+    , il.ingredients
+    , COALESCE(tl.tags, '[]') as tags
+    FROM Recipes r
+    INNER JOIN IngredientLists il ON il.recipeId = r.id
+    INNER JOIN TagList tl ON tl.recipeId = r.id
+    """
 
-  var recipes: seq[Recipe] = @[]
-  for row in db_conn.fastRows(getRecipesQuery):
-    let recipe = Recipe(
-      id: row[0].parseInt,
-      title: row[1],
-      preparationTime: row[2].parseInt,
-      instructions: row[3].splitLines,
-      servings: row[4].parseInt,
-      ingredients: parseJson(row[5]).to(seq[Ingredient]),
-      tags: parseJson(row[6]).to(seq[Tag])
+    var recipes: seq[Recipe] = @[]
+    for row in db_conn.fastRows(getRecipesQuery):
+      let recipe = Recipe(
+        id: row[0].parseInt,
+        title: row[1],
+        preparationTime: row[2].parseInt,
+        instructions: row[3].splitLines,
+        servings: row[4].parseInt,
+        ingredients: parseJson(row[5]).to(seq[Ingredient]),
+        tags: parseJson(row[6]).to(seq[Tag])
+      )
+
+      recipes.add recipe
+
+    return recipes
+
+proc addRecipe*(recipe: Recipe) =
+  withDb db_conn:
+    let addRecipeQuery = sql"""
+    INSERT INTO Recipes (
+      title,
+      instructions,
+      timeInMinutes,
+      servings
+    )
+    VALUES (
+      ?,
+      ?,
+      ?,
+      ?
+    )
+    """
+    let recipeId = db_conn.insertId(addRecipeQuery,
+      recipe.title,
+      recipe.instructions.join("\n"),
+      recipe.preparationTime,
+      recipe.servings
     )
 
-    recipes.add recipe
+    let addIngredientQuery = sql"""
+      INSERT INTO Ingredients (
+        name,
+        recipeId,
+        amount,
+        unit
+      ) VALUES (
+        ?,
+        ?,
+        ?,
+        ?
+      )
+    """
+    for ingredient in recipe.ingredients:
+      discard db_conn.insertId(addIngredientQuery,
+        ingredient.name,
+        recipeId,
+        ingredient.amount,
+        ingredient.unit
+      )
+    
+    # Add tag if it doesn't already exist
+    let addTagQuery = sql"""
+      INSERT OR IGNORE INTO Tags (name)
+      VALUES (?)
+      RETURNING id
+    """
+    let addRecipeTagQuery = sql"""
+      INSERT INTO RecipeHasTag (
+        recipeId,
+        tagId
+      ) VALUES (
+        ?,
+        ?
+      )
+    """
 
-  db_conn.close()
+    for tag in recipe.tags:
+      let tagId = db_conn.getRow(addTagQuery, tag.name)[0].parseInt
+      discard db_conn.insertId(addRecipeTagQuery, recipeId, tagId)
 
-  return recipes
+    
